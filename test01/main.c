8a01697b18fe86d2cbc7efcd16c158323d440843
CzhHHHHHHHHHHHH <2682582282@qq.com>
2024-02-19 17:51:05 +0800

    test01

---
+++ test01/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+    int n=10;
+    int m=20;
+    int * const p=&m;
+    //p=&n;
+    
+    return 0;
+}
+
+
+
+
+// int main()
+// {
+//     int i = 0;
+//     int arr[10] = {0};
+//     for (i = 0; i <= 12; i++)
+//     {
+//         arr[i] = 0;
+//         printf("hehe\n");
+//     }
+//     return 0;
+// }
