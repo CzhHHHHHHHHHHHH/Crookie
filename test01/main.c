5b00eb398169a853ca237cb2849a0a5e2f074374
CzhHHHHHHHHHHHH <2682582282@qq.com>
2024-02-20 20:00:04 +0800

    C07_24-2-20

--- test01/main.c
+++ test01/main.c
@@ -1,17 +1,63 @@
 #include <stdio.h>
+#include <assert.h>
 
 int main()
 {
-    int n=10;
-    int m=20;
-    int * const p=&m;
-    //p=&n;
-    
+    char a=128;
+    printf("%d",a+128);
     return 0;
 }
 
+// int my_strlen(const char* arr)
+// {
+//     assert(arr != NULL);
+//     int count=0;
+//     while(*arr !='\0')
+//     {
+//         count++;
+//         arr++;
+//     }
+//     return count;
+// }
 
+// //my_strlen
+// int main()
+// {
+//     char arr[20];
+//     printf("输入一个字符串：");
+//     scanf("%s",arr);
+//     int len=my_strlen(arr);
+//     printf("%d",len);
+//     return 0;
+// }
 
+// char* my_strcpy(char* dest,const char* src )
+// {
+//     assert(dest != NULL);
+//     assert(src != NULL);//拦截空指针
+//     char* ret=dest;
+//     while (*dest++=*src++)//依次替换
+//     {
+//         //*dest++=*src++;
+//         // *dest=*src;
+//         // dest++;
+//         // src++;
+//         ;
+//     }
+//     //*dest=*src;
+//     return ret;
+// }
+
+//my_strcpy
+// int main()
+// {
+//     char arr1[]="************";
+//     char arr2[]="Hello World!";
+
+//     char *ret =my_strcpy(arr1,arr2);
+//     printf("%s",ret);
+//     return 0;
+// }
 
 // int main()
 // {
